b005ff5747f2799e360344af7659fc7f18ff7b6e
Miles Anderson <milesanderson@Miless-MacBook-Pro-8.local>
2026-03-06 02:18:22 -0800

    implemented Random function from text and frequency test for testing randomness

--- algorithm1.py
+++ algorithm1.py
@@ -1,12 +1,15 @@
 
-def algorithm1(seed, n):
-    i = 1
-    l = []
+import random
+
+def Random(n):
+    i = 1.0
     x = []
-    l.append(seed)
+    k = 16807
+    j = 2147483647
+    seed = random.randint(1, j)
     while i <= n:
-        l.append(((7**5) * l[i-1]) % 2147483647) #2^31 - 1 = 2147483647
-        x.append(l[i] / 2147483647)
+        seed = (k * seed) % j
+        x.append(seed / j)
         i = i+1
     return x
     
--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import time
+import numpy as np
+import math
+import scipy
 
 import algorithm1
+import freq_test
 
+'''
+#time Random
 start = time.time()
-algorithm1.algorithm1(32545, 1000)
+algorithm1.Random(1000)
 end = time.time()
 alg1_time = end - start
 
-print("Algorithm1 run time: ", alg1_time)
+print(alg1_time)
+'''
+
+x = algorithm1.Random(1000000)
+isRand, p_value = freq_test.freq_test(x)
+print(isRand)
+print(p_value)
